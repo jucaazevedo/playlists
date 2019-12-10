0fcb834ff4abc7aaaf3e021090ea98f77ecf2c58
Joaquim Azevedo <jazevedo@simplesmenteuse.com>
2019-12-10 15:17:16 -0300

    backup dir

---
+++ 20191208/gera_lista.sh
@@ -0,0 +1,67 @@
+sed -e "s/$/,Art Blakey/" standards_essenciais.txt >> standards_essenciais_do_jazz.csv
+sed -e "s/$/,Art Pepper /" standards_essenciais.txt >> standards_essenciais_do_jazz.csv
+sed -e "s/$/,Art Tatum/" standards_essenciais.txt >> standards_essenciais_do_jazz.csv
+sed -e "s/$/,Art Van Damme/" standards_essenciais.txt >> standards_essenciais_do_jazz.csv
+sed -e "s/$/,Ben Webster /" standards_essenciais.txt >> standards_essenciais_do_jazz.csv
+sed -e "s/$/,Benny Golson /" standards_essenciais.txt >> standards_essenciais_do_jazz.csv
+sed -e "s/$/,Benny Goodman/" standards_essenciais.txt >> standards_essenciais_do_jazz.csv
+sed -e "s/$/,Bill Evans/" standards_essenciais.txt >> standards_essenciais_do_jazz.csv
+sed -e "s/$/,Billie Holiday/" standards_essenciais.txt >> standards_essenciais_do_jazz.csv
+sed -e "s/$/,Brad Mehldau/" standards_essenciais.txt >> standards_essenciais_do_jazz.csv
+sed -e "s/$/,Bud Powell/" standards_essenciais.txt >> standards_essenciais_do_jazz.csv
+sed -e "s/$/,Cecil Taylor/" standards_essenciais.txt >> standards_essenciais_do_jazz.csv
+sed -e "s/$/,Charlie Parker/" standards_essenciais.txt >> standards_essenciais_do_jazz.csv
+sed -e "s/$/,Chet Baker/" standards_essenciais.txt >> standards_essenciais_do_jazz.csv
+sed -e "s/$/,Chick Corea/" standards_essenciais.txt >> standards_essenciais_do_jazz.csv
+sed -e "s/$/,Chucho Valdes /" standards_essenciais.txt >> standards_essenciais_do_jazz.csv
+sed -e "s/$/,Clifford Brown/" standards_essenciais.txt >> standards_essenciais_do_jazz.csv
+sed -e "s/$/,Coleman Hawkins/" standards_essenciais.txt >> standards_essenciais_do_jazz.csv
+sed -e "s/$/,Count Basie/" standards_essenciais.txt >> standards_essenciais_do_jazz.csv
+sed -e "s/$/,Courtney Pine/" standards_essenciais.txt >> standards_essenciais_do_jazz.csv
+sed -e "s/$/,Dave Brubeck /" standards_essenciais.txt >> standards_essenciais_do_jazz.csv
+sed -e "s/$/,Dexter Gordon /" standards_essenciais.txt >> standards_essenciais_do_jazz.csv
+sed -e "s/$/,Dinah Washington/" standards_essenciais.txt >> standards_essenciais_do_jazz.csv
+sed -e "s/$/,Dizzy Gillespie/" standards_essenciais.txt >> standards_essenciais_do_jazz.csv
+sed -e "s/$/,Don Cherry/" standards_essenciais.txt >> standards_essenciais_do_jazz.csv
+sed -e "s/$/,Duke Ellington/" standards_essenciais.txt >> standards_essenciais_do_jazz.csv
+sed -e "s/$/,Earl Hines/" standards_essenciais.txt >> standards_essenciais_do_jazz.csv
+sed -e "s/$/,Ella Fitzgerald/" standards_essenciais.txt >> standards_essenciais_do_jazz.csv
+sed -e "s/$/,Emily Bear/" standards_essenciais.txt >> standards_essenciais_do_jazz.csv
+sed -e "s/$/,Erroll Garner/" standards_essenciais.txt >> standards_essenciais_do_jazz.csv
+sed -e "s/$/,Frank Sinatra/" standards_essenciais.txt >> standards_essenciais_do_jazz.csv
+sed -e "s/$/,Herbie Hancock/" standards_essenciais.txt >> standards_essenciais_do_jazz.csv
+sed -e "s/$/,Horace Silver /" standards_essenciais.txt >> standards_essenciais_do_jazz.csv
+sed -e "s/$/,J J Johnson/" standards_essenciais.txt >> standards_essenciais_do_jazz.csv
+sed -e "s/$/,Jason Moran/" standards_essenciais.txt >> standards_essenciais_do_jazz.csv
+sed -e "s/$/,Jim Hall /" standards_essenciais.txt >> standards_essenciais_do_jazz.csv
+sed -e "s/$/,Joe Pass/" standards_essenciais.txt >> standards_essenciais_do_jazz.csv
+sed -e "s/$/,Joey Alexander/" standards_essenciais.txt >> standards_essenciais_do_jazz.csv
+sed -e "s/$/,John Pizzarelli/" standards_essenciais.txt >> standards_essenciais_do_jazz.csv
+sed -e "s/$/,John Coltrane/" standards_essenciais.txt >> standards_essenciais_do_jazz.csv
+sed -e "s/$/,Keith Jarrett/" standards_essenciais.txt >> standards_essenciais_do_jazz.csv
+sed -e "s/$/,Lee Konitz /" standards_essenciais.txt >> standards_essenciais_do_jazz.csv
+sed -e "s/$/,Lester Young/" standards_essenciais.txt >> standards_essenciais_do_jazz.csv
+sed -e "s/$/,Lionel Hampton/" standards_essenciais.txt >> standards_essenciais_do_jazz.csv
+sed -e "s/$/,Louis Armstrong/" standards_essenciais.txt >> standards_essenciais_do_jazz.csv
+sed -e "s/$/,Michel Petrucciani/" standards_essenciais.txt >> standards_essenciais_do_jazz.csv
+sed -e "s/$/,Miles Davis/" standards_essenciais.txt >> standards_essenciais_do_jazz.csv
+sed -e "s/$/,Milt Jackson/" standards_essenciais.txt >> standards_essenciais_do_jazz.csv
+sed -e "s/$/,Nat King Cole/" standards_essenciais.txt >> standards_essenciais_do_jazz.csv
+sed -e "s/$/,Nina Simone/" standards_essenciais.txt >> standards_essenciais_do_jazz.csv
+sed -e "s/$/,Oscar Peterson /" standards_essenciais.txt >> standards_essenciais_do_jazz.csv
+sed -e "s/$/,Paulo Moura /" standards_essenciais.txt >> standards_essenciais_do_jazz.csv
+sed -e "s/$/,Phil Woods /" standards_essenciais.txt >> standards_essenciais_do_jazz.csv
+sed -e "s/$/,Ray Charles/" standards_essenciais.txt >> standards_essenciais_do_jazz.csv
+sed -e "s/$/,Red Garland/" standards_essenciais.txt >> standards_essenciais_do_jazz.csv
+sed -e "s/$/,Red Norvo/" standards_essenciais.txt >> standards_essenciais_do_jazz.csv
+sed -e "s/$/,Roy Hargrove/" standards_essenciais.txt >> standards_essenciais_do_jazz.csv
+sed -e "s/$/,Sarah Vaughan /" standards_essenciais.txt >> standards_essenciais_do_jazz.csv
+sed -e "s/$/,Sonny Rollins/" standards_essenciais.txt >> standards_essenciais_do_jazz.csv
+sed -e "s/$/,Sonny Stitt /" standards_essenciais.txt >> standards_essenciais_do_jazz.csv
+sed -e "s/$/,Stan Kenton/" standards_essenciais.txt >> standards_essenciais_do_jazz.csv
+sed -e "s/$/,The Modern Jazz Quartet/" standards_essenciais.txt >> standards_essenciais_do_jazz.csv
+sed -e "s/$/,Thelonious Monk /" standards_essenciais.txt >> standards_essenciais_do_jazz.csv
+sed -e "s/$/,Wes Montgomery/" standards_essenciais.txt >> standards_essenciais_do_jazz.csv
+sed -e "s/$/,Woody Herman/" standards_essenciais.txt >> standards_essenciais_do_jazz.csv
+sed -e "s/$/,Wynton Marsalis/" standards_essenciais.txt >> standards_essenciais_do_jazz.csv
+sed -e "s/$/,Zoot Sims /" standards_essenciais.txt >> standards_essenciais_do_jazz.csv
